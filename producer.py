1629526eddf73c41ae82891e5900588ab96b8924
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-19 23:59:23 +0200

    Add files via upload

---
+++ producer.py
@@ -0,0 +1,39 @@
+from RPA.Browser.Selenium import Selenium 
+import re
+import time
+import requests
+import logging
+from pathlib import Path
+from robocorp import vault
+from robocorp import excel
+from robocorp import storage
+from datetime import datetime
+from robocorp.tasks import task
+from robocorp import workitems
+# from robocorp.workitems import WorkItems
+from datetime import datetime, timedelta
+from robocorp.tasks import get_output_dir
+from browser_manager import BrowserManager as BM 
+from data_retriever import DataRetriever 
+
+@task
+def producer():
+
+    
+    bm = BM()
+    url = "https://www.aljazeera.com/"
+    bm.opening_the_news_Site(url)
+    bm.search_the_phrase("Business")
+    dr = DataRetriever(bm)
+    dr.retrive_data(1, "Business")
+
+    # Close the browser
+    # bm.close_browser()
+
+
+    # save_data_to_Excel(workbook, sheet_name)
+    # workbook.save(excel_file_path)
+
+    # Saving the workbook
+    # workbook.save(excel_file_path)
+    
