cc55b7e93e2cf2a08c8da3b12137d7a52814526f
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-20 04:20:20 +0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -16,30 +16,15 @@ from robocorp.tasks import get_output_dir
 from browser_manager import BrowserManager as BM 
 from data_retriever import DataRetriever 
 
-bm = BM()
-url = "https://www.aljazeera.com/"
-sp = "Business"
-number_of_months = 1
-
-@task
-def opening_site():
-    global bm
-    global url
+def main():
+    bm = BM()
+    url = "https://www.aljazeera.com/"
+    sp = "Business"
+    number_of_months = 1
     bm.opening_the_news_Site(url)
-
-@task
-def searching_phrase():
-    global bm
-    global sp
     bm.search_the_phrase(sp)
-@task
-def extract_data():
-    global bm
-    global sp
-    global number_of_months
     dr = DataRetriever(bm)
     dr.retrive_data(number_of_months, sp)
-    
 def close_the_browser():
     global bm
     # Close the browser
